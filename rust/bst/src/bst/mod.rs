78e31a013d0adfaca4bd4d5965df55454c84d672
hungrymonkey <boringmonkey@gmail.com>
2017-08-24 23:55:56 -0700

    compiling

--- rust/bst/src/bst/mod.rs
+++ rust/bst/src/bst/mod.rs
@@ -1,6 +1,36 @@
+use std::fmt;
+
 mod node;
-use self::node::Node;
+use self::node::Node as Node;
+
 pub struct BST<T> {
    size: u32,
    root: Node<T>
 }
+
+impl <T: Ord> BST<T> {
+   pub fn new() -> BST<T> {
+      BST{ size: 0, root: Node::Nil }
+   }
+   pub fn insert(&mut self, v: T) -> bool {
+      if self.empty() {
+          self.root = Node::Leaf(v);
+          self.size = self.size + 1;
+          return true;
+      }
+      return false;
+          
+      
+   }
+   pub fn empty(&self) -> bool {
+      self.size == 0
+   }
+}
+
+impl <T: fmt::Display> fmt::Display for BST<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.root)
+    }
+}
+
+
--- rust/bst/src/bst/node.rs
+++ rust/bst/src/bst/node.rs
@@ -1,5 +1,25 @@
+use std::fmt;
+
 pub enum Node<T> {
-   Node { v: T, p: Box<Node<T>> , l: Box<Node<T>>, r: Box<Node<T>> },
-   None
+   Leaf(T),
+   Node(T, Box<Node<T>>, Box<Node<T>>, Box<Node<T>>),
+   Nil
+}
+
+
+impl <T> Node<T> {
+   pub fn new(v: T) -> Node<T>{
+      Node::Leaf(v)
+   }
+   
+}
+impl <T: fmt::Display> fmt::Display for Node<T>{
+   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      match *self {
+         Node::Leaf(ref d) => write!(f, "{},", d),
+         Node::Node(ref d, _, ref l, ref r) => write!(f, "{}{},{}", l, d, r),
+         Node::Nil => write!(f, "{}", "")
+      }
+   }
 }
 
