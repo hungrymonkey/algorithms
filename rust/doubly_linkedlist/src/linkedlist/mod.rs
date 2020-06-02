f5837208ae27bc717ba50d7dad16cc3ca157b827
hungrymonkey <boringmonkey@gmail.com>
2020-06-02 04:52:51 -0700

    Added test code for the linkedlist
    rustc main.rs
    
    thread 'main' panicked at 'already borrowed: BorrowMutError', /private/tmp/rust-20200509-70766-vc2112/rustc-1.43.1-src/src/libcore/cell.rs:878:9
    stack backtrace:
       0: <std::sys_common::backtrace::_print::DisplayBacktrace as core::fmt::Display>::fmt
       1: core::fmt::write
       2: std::io::Write::write_fmt
       3: std::panicking::default_hook::{{closure}}
       4: std::panicking::default_hook
       5: std::panicking::rust_panic_with_hook
       6: rust_begin_unwind
       7: core::panicking::panic_fmt
       8: core::result::unwrap_failed
       9: core::result::Result<T,E>::expect
      10: core::cell::RefCell<T>::borrow_mut
      11: main::linkedlist::Cons<T>::append
      12: main::linkedlist::LinkedList<T>::append
      13: main::main
      14: std::rt::lang_start::{{closure}}
      15: std::panicking::try::do_call
      16: __rust_maybe_catch_panic
      17: std::rt::lang_start_internal
      18: std::rt::lang_start
      19: main
    note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.

--- rust/doubly_linkedlist/src/linkedlist/mod.rs
+++ rust/doubly_linkedlist/src/linkedlist/mod.rs
@@ -1,18 +1,18 @@
 use std::fmt;
-
+use std::fmt::{Display};
 use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
 pub struct Cons<T> {
 	//Cons( Data, next, prev)
-	pub entry: T,
+	pub data: T,
 	pub prev: Option<Weak<RefCell<Cons<T>>>>,
 	pub next: Option<Rc<RefCell<Cons<T>>>>,
 }
 
-impl <T> Cons<T> {
+impl<T> Cons<T> {
 	pub fn new(v: T) -> Self {
-		Self { entry: v, prev: None, next: None }
+		Self { data: v, prev: None, next: None }
 	}
 	
 	pub fn append(prev: &mut Rc<RefCell<Cons<T>>>, data: T) -> Option<Rc<RefCell<Cons<T>>>> {
@@ -20,7 +20,7 @@ impl <T> Cons<T> {
 		// Check if the node is a tail
 		match &mut prev.borrow_mut().next {
 			Some(next) => {
-				let mut e = Self { entry: data, prev: Some(Rc::downgrade(&prev)), next: Some(next.clone()) };
+				let mut e = Self { data: data, prev: Some(Rc::downgrade(&prev)), next: Some(next.clone()) };
 				let rc = Rc::new(RefCell::new(e));
 				prev.borrow_mut().next = Some(rc.clone());
 				next.borrow_mut().prev = Some(Rc::downgrade(&rc));
@@ -29,7 +29,7 @@ impl <T> Cons<T> {
 			None => {
 				//let mut e = Cons::new(data);
 				//e.prev = Some(Rc::downgrade(&prev));
-				let mut e = Self { entry: data, prev: Some(Rc::downgrade(&prev)), next: None };
+				let mut e = Self { data: data, prev: Some(Rc::downgrade(&prev)), next: None };
 				let rc = Rc::new(RefCell::new(e));
 				prev.borrow_mut().next = Some(rc.clone());
 				Some(rc)
@@ -47,14 +47,29 @@ impl <T> LinkedList<T> {
 	pub fn new() -> Self {
 		Self { head: None, tail: None }
 	}
-	pub fn append( &mut self, entry: T ) {
+	pub fn append( &mut self, data: T ) {
 		match &mut self.tail {
-			Some(tail) => { Cons::append(tail, entry); }
+			Some(tail) => { Cons::append(tail, data); }
 			None => {
-				let e = Rc::new(RefCell::new(Cons::new(entry)));
+				let e = Rc::new(RefCell::new(Cons::new(data)));
 				self.head = Some(e.clone());
             	self.tail = Some(e);
 			}
 		}
+	}
+}
+
+impl<T: Display> Display for LinkedList<T> {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+		write!(f, "[");
+		let mut data = self.head.clone();
+		while let Some(e) = data {
+			write!(f, "{}", e.borrow().data);
+			data = e.borrow().next.clone();
+			if data.is_some() {
+				write!(f, ", ");
+			}
+		}
+		write!(f, "]")
 	}
 }
--- rust/doubly_linkedlist/src/main.rs
+++ rust/doubly_linkedlist/src/main.rs
@@ -2,5 +2,10 @@ mod linkedlist;
 use linkedlist::LinkedList as LinkedList;
 
 fn main() {
-    println!("Hello, world!");
+    let mut list = LinkedList::new();
+    println!("{}", list);
+    for i in 1..10 {
+        list.append(i);
+    }
+    println!("{}", list);
 }
