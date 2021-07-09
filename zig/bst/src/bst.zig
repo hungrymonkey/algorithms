d9a7dea5f6cbb70432dfe1adf53bd1bdff259c6a
hungrymonkey <boringmonkey@gmail.com>
2021-07-09 04:33:26 -0700

    Remove parent pointer
    Created recurive insert and print walk
    
    TODO:
    - Test insert
    - Test print
    - Implement Serialize
    - Implement Deserialize
    
    BUILD TODO:
    Meson
    zig.build

--- zig/bst/src/bst.zig
+++ zig/bst/src/bst.zig
@@ -1,6 +1,7 @@
+const std = @import("std");
+
 const Node = struct {
     val: i32,
-    p: ?*Node,
     l: ?*Node,
     r: ?*Node,
 };
@@ -11,10 +12,26 @@ const BST = struct {
 
 fn bin_insert(b: *BST, v: i32) *Node {
     if (b.root == null) {
-        b.root = Node{ .val = v, .p = null, .l = null, .r = null };
+        b.root = Node{ .val = v, .l = null, .r = null };
         return b.root;
     } else {
         return node_insert(b.root, v);
     }
 }
-fn node_insert(curr: *Node, v: i32) *Node {}
+fn node_insert(curr: *Node, v: i32) *Node {
+    if (curr == null) {
+        curr = Node{ .val = v, .l = null, .r = null };
+        return curr;
+    }
+    if (curr.val < v) {
+        return node_insert(curr.r, v);
+    } else {
+        return node_insert(curr.l, v);
+    }
+}
+fn print_bin(b: *BST) void {
+    if(b == null) return;
+    print_bin(b.l);
+    try stdout.print("Hello, {d}!\n", .{b.val});
+    print_bin(b.r);
+}
